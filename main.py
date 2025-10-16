294f48a103ce8f55922b2ab77473d13915e80165
michailenkomaks <michailenkomaks@gmail.com>
2025-10-16 17:18:23 +0300

    переменные

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello max")
+name = 'max'
+age = 21
+family = ['elena','evgenii','ksenia']
+print (f'Тип name:{type(name)}')
+print (f'Тип age:{type(age)}')
+print (f'Тип family:{type(family)}')
